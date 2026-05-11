6e2a61f8326d9631d9d4f36bd55fe7fa4e09406a
Marwa Harhouri <harhourimarwa97@gmail.com>
2026-05-11 14:14:13 +0100

    update

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -1,32 +1,64 @@
+//package Helper;
+//
+//import java.time.Duration;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+//public class Config {
+//	
+//	public static WebDriver driver;
+//    
+//	public static void maximizewindow() {
+//		
+//		driver.manage().window().maximize();
+//	}
+//	 public static void confChrome() {
+//		 
+//	     driver = new ChromeDriver(); 
+//
+//	        driver.manage().window().maximize();
+//
+//	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//	    }
+//	 
+//
+//	public static void attente (int s) {
+//		
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
+//	}
+//	
+//
+//}
+
 package Helper;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Config {
-	
-	public static WebDriver driver;
-    
-	public static void maximizewindow() {
-		
-		driver.manage().window().maximize();
-	}
-	 public static void confChrome() {
-		 
-	     driver = new ChromeDriver(); 
 
-	        driver.manage().window().maximize();
+    public static WebDriver driver;
 
-	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	    }
-	 
+    public static void confChrome() {
 
-	public static void attente (int s) {
-		
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
-	}
-	
+        WebDriverManager.chromedriver().setup(); 
 
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    public static void maximizewindow() {
+        driver.manage().window().maximize();
+    }
+
+    public static void attente(int s) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
+    }
 }
+
