121cb3d3370a556c0f7dd3b4edded2712bb25229
Marwa Harhouri <harhourimarwa97@gmail.com>
2026-06-10 15:11:40 +0100

    Refactor getProperty method to use classloader
    Updated the method to load properties from the classpath instead of a file path and added a null check for the input stream.

--- src/test/java/Helper/Utils.java
+++ src/test/java/Helper/Utils.java
@@ -1,6 +1,25 @@
+// package Helper;
+
+// import java.io.FileInputStream;
+// import java.io.InputStream;
+// import java.util.Properties;
+
+// public class Utils {
+
+//     public static String getProperty(String property) throws Exception {
+
+//         InputStream input = new FileInputStream("src/test/resources/Properties/properties");
+
+//         Properties properties = new Properties();
+        
+//         properties.load(input);
+
+//         return properties.getProperty(property);
+//     }
+// }
+
 package Helper;
 
-import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
@@ -8,7 +27,13 @@ public class Utils {
 
     public static String getProperty(String property) throws Exception {
 
-        InputStream input = new FileInputStream("src/test/resources/Properties/properties");
+        InputStream input = Utils.class
+                .getClassLoader()
+                .getResourceAsStream("Properties/properties");
+
+        if (input == null) {
+            throw new RuntimeException("Fichier properties introuvable !");
+        }
 
         Properties properties = new Properties();
         properties.load(input);
