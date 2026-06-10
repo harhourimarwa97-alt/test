7d9287a7c382d60f91378183d89568894fac1c04
Marwa Harhouri <harhourimarwa97@gmail.com>
2026-06-10 10:32:08 +0100

    Refactor WebDriver management in Config class
    Refactored driver management to use instance variable and added methods for starting and quitting the driver.

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -1,36 +1,31 @@
 package Helper;
 
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Config {
 
-    public static WebDriver driver;
+    public WebDriver driver;
 
-    public static void maximizewindow() {
-        driver.manage().window().maximize();
-    }
+    public WebDriver startDriver() {
 
-    public static void confChrome() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-extensions");
-        options.addArguments("--disable-software-rasterizer");
 
-        driver = new ChromeDriver(options);  // ← driver créé avec tous les flags
-
+        driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        return driver;
     }
 
-    public static void attente(int s) {
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
+    public void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
 }
