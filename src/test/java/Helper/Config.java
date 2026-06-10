9c132ac8f48ef045800e1906688a8676e44f5152
Marwa Harhouri <harhourimarwa97@gmail.com>
2026-06-10 10:36:23 +0100

    Refactor Config class for static WebDriver usage
    Refactor Config class to use static WebDriver and add methods for maximizing the window and setting implicit wait.

--- src/test/java/Helper/Config.java
+++ src/test/java/Helper/Config.java
@@ -1,31 +1,36 @@
 package Helper;
 
 import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class Config {
 
-    public WebDriver driver;
+    public static WebDriver driver;
 
-    public WebDriver startDriver() {
+    public static void maximizewindow() {
+        driver.manage().window().maximize();
+    }
 
+    public static void confChrome() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
         options.addArguments("--window-size=1920,1080");
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-extensions");
+        options.addArguments("--disable-software-rasterizer");
 
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver(options);  // ← driver créé avec tous les flags
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        return driver;
     }
 
-    public void quitDriver() {
-        if (driver != null) {
-            driver.quit();
-        }
+    public static void attente(int s) {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(s));
     }
 }
