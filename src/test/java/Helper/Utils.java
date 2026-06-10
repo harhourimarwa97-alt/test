26d087810b165ded5ccf4dc7cc6cff00ae7a9a73
Marwa Harhouri <harhourimarwa97@gmail.com>
2026-06-10 14:57:49 +0100

    Refactor getProperty method in Utils class

--- src/test/java/Helper/Utils.java
+++ src/test/java/Helper/Utils.java
@@ -5,15 +5,14 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Utils {
-	public static String getProperty(String property) throws Exception {
-		
-	InputStream input = new FileInputStream("src\\test\\resources\\Properties\\properties");
-	
-	Properties properties = new Properties();
-	properties.load(input);
-	
-	return(properties.getProperty(property));
 
-}
+    public static String getProperty(String property) throws Exception {
 
+        InputStream input = new FileInputStream("src/test/resources/Properties/properties");
+
+        Properties properties = new Properties();
+        properties.load(input);
+
+        return properties.getProperty(property);
+    }
 }
