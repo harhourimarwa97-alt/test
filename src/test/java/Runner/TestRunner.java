d3d3ba7f3e85d9e0b06a74fd565123edcdbc6f05
Marwa Harhouri <harhourimarwa97@gmail.com>
2026-06-22 15:20:31 +0100

    Update feature path in TestRunner to specific file

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-    features = "src/test/resources/Features",
+    features = "src/test/resources/Features/login.feature",
     glue = "StepDefErudaxis",
     tags = "",
     plugin = {
